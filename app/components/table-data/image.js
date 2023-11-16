f1cdf7e3e7d55448547ad38d09688bfed287a6d3
JuanPabloZ <juanpablo.zunigah@gmail.com>
2023-11-16 12:46:08 +0100

    table display ability ready

--- app/components/table-data/image.js
+++ app/components/table-data/image.js
@@ -3,7 +3,6 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class RentalImageComponent extends Component {
-
   @tracked isLarge = false;
 
   @action toggleSize() {
--- app/components/transaction-form.js
+++ app/components/transaction-form.js
@@ -7,7 +7,6 @@ export default Component.extend({
   accountNumber: null,
   transactionAmount: null,
   message: '',
-  
 
   init() {
     this._super(...arguments);
@@ -16,24 +15,27 @@ export default Component.extend({
     this.submitTransaction = this.submitTransaction.bind(this);
     this.fetchAccountsFromJsonBin = this.fetchAccountsFromJsonBin.bind(this);
     this.postDataToJsonBin = this.postDataToJsonBin.bind(this);
-    this.postDataToJsonBinTransactions = this.postDataToJsonBinTransactions.bind(this);
+    this.postDataToJsonBinTransactions =
+      this.postDataToJsonBinTransactions.bind(this);
   },
 
   submitTransaction: async function (event) {
     event.preventDefault();
-  
-    const accountNumber = this.get('accountNumber');
-    const transactionAmount = parseFloat(this.get('transactionAmount'));
-  
+
+    const accountNumber = this.accountNumber;
+    const transactionAmount = parseFloat(this.transactionAmount);
+
     // Fetch accounts from jsonbin.io
-    const accounts = await this.fetchAccountsFromJsonBin('654ce74612a5d3765997106e');
+    const accounts = await this.fetchAccountsFromJsonBin(
+      '654ce74612a5d3765997106e',
+    );
     console.log(accountNumber);
     console.log(transactionAmount);
     console.log(accounts);
-  
+
     // Validate account existence
     let account = accounts.find((acc) => acc.account_number === accountNumber);
-  
+
     if (!account) {
       // If account not found, create a new account
       this.set('message', 'Account not found');
@@ -44,7 +46,7 @@ export default Component.extend({
         running_balance: transactionAmount, // Initialize running_balance for new accounts
         latest_transaction_amount: transactionAmount, // Initialize latest_transaction_amount for new accounts
       };
-  
+
       // Add the very first transaction to the transaction history
       const firstTransaction = {
         transaction_id: `t${Date.now()}`,
@@ -53,7 +55,7 @@ export default Component.extend({
         transaction_amount: transactionAmount,
         updated_balance: transactionAmount,
       };
-  
+
       // Add the new account to the accounts array
       account.transaction_history.push(firstTransaction);
       accounts.push(account);
@@ -66,13 +68,13 @@ export default Component.extend({
         transaction_amount: transactionAmount,
         updated_balance: account.running_balance + transactionAmount,
       };
-  
+
       // Update the running balance for the account
       account.running_balance += transactionAmount;
-  
+
       // Update the latest_transaction_amount for the account
       account.latest_transaction_amount = transactionAmount;
-  
+
       // Add the transaction to the transaction history
       account.transaction_history.push(transaction);
     }
@@ -81,21 +83,24 @@ export default Component.extend({
     await this.postDataToJsonBin({ accounts }, '654ce74612a5d3765997106e');
 
     // Push transaction to jsonbin.io for transaction history
-    await this.postDataToJsonBinTransactions({
-      transaction_id: `t${Date.now()}`,
-      date_time: new Date().toISOString(),
-      account_number: accountNumber,
-      transaction_amount: transactionAmount,
-      updated_balance: account.running_balance,
-    }, '654ce85612a5d376599710d9');
-  
+    await this.postDataToJsonBinTransactions(
+      {
+        transaction_id: `t${Date.now()}`,
+        date_time: new Date().toISOString(),
+        account_number: accountNumber,
+        transaction_amount: transactionAmount,
+        updated_balance: account.running_balance,
+      },
+      '654ce85612a5d376599710d9',
+    );
+
     // Show success message
     this.set('message', 'Transaction made');
-  
+
     // Clear form
     this.set('accountNumber', null);
     this.set('transactionAmount', null);
-  
+
     // Clear message after 2 seconds
     setTimeout(() => {
       this.set('message', '');
@@ -103,12 +108,13 @@ export default Component.extend({
   },
 
   fetchAccountsFromJsonBin: async function (binId) {
-    const apiKey = '$2b$10$xFC7BlC/9mfhK2jwRMo.IemTR8HRFha0TZyWFgA8n./iRCF2kjqpG';
+    const apiKey =
+      '$2b$10$xFC7BlC/9mfhK2jwRMo.IemTR8HRFha0TZyWFgA8n./iRCF2kjqpG';
     const apiUrl = `https://api.jsonbin.io/v3/b/${binId}/latest`;
-  
+
     return new Promise((resolve, reject) => {
       const req = new XMLHttpRequest();
-  
+
       req.onreadystatechange = () => {
         if (req.readyState === XMLHttpRequest.DONE) {
           if (req.status === 200) {
@@ -121,12 +127,15 @@ export default Component.extend({
               reject([]);
             }
           } else {
-            console.error('Error fetching accounts from jsonbin.io:', req.statusText);
+            console.error(
+              'Error fetching accounts from jsonbin.io:',
+              req.statusText,
+            );
             reject([]);
           }
         }
       };
-  
+
       req.open('GET', apiUrl, true);
       req.setRequestHeader('X-Master-Key', apiKey);
       req.send();
@@ -134,7 +143,8 @@ export default Component.extend({
   },
 
   postDataToJsonBin: async function (data, binId) {
-    const apiKey = '$2b$10$xFC7BlC/9mfhK2jwRMo.IemTR8HRFha0TZyWFgA8n./iRCF2kjqpG';
+    const apiKey =
+      '$2b$10$xFC7BlC/9mfhK2jwRMo.IemTR8HRFha0TZyWFgA8n./iRCF2kjqpG';
     const apiUrl = `https://api.jsonbin.io/v3/b/${binId}`;
 
     try {
@@ -152,9 +162,10 @@ export default Component.extend({
   },
 
   postDataToJsonBinTransactions: async function (newTransaction, binId) {
-    const apiKey = '$2b$10$xFC7BlC/9mfhK2jwRMo.IemTR8HRFha0TZyWFgA8n./iRCF2kjqpG';
+    const apiKey =
+      '$2b$10$xFC7BlC/9mfhK2jwRMo.IemTR8HRFha0TZyWFgA8n./iRCF2kjqpG';
     const apiUrl = `https://api.jsonbin.io/v3/b/${binId}`;
-  
+
     try {
       // Fetch existing data from the JSON bin
       const existingData = await fetch(apiUrl, {
@@ -162,14 +173,15 @@ export default Component.extend({
         headers: {
           'X-Master-Key': apiKey,
         },
-      }).then(response => response.json());
-  
+      }).then((response) => response.json());
+
       // Extract the existing transactions array or initialize an empty array
-      const transactions = existingData.record && existingData.record.transactions || [];
-  
+      const transactions =
+        (existingData.record && existingData.record.transactions) || [];
+
       // Append the new transaction to the existing array
       transactions.push(newTransaction);
-  
+
       // Update the JSON bin with the modified transactions array
       await fetch(apiUrl, {
         method: 'PUT',
@@ -183,8 +195,4 @@ export default Component.extend({
       console.error('Error posting data to jsonbin.io:', error);
     }
   },
-
-
-
 });
-
